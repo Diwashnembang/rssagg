f2864efcf07e1d14441cd1c17220c78d35c57ccd
Diwash Nembang <nembangd0@gmail.com>
2024-09-17 08:15:48 +1000

    boilerplate for http server complete

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loaing .env file ", err)
+	}
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("port is not found")
+	}
+	router := chi.NewRouter() // this creates a new http server router  and retutuns pointer to fo chi.mux which is a struct that has the server i think
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerRediness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Print("Servert starting on port on :", portString)
+	srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
